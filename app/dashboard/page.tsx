3c5889ffbea4d5bc26f12fde1954e967f9a120d7
Yurii2108 <urapredger@gmail.com>
2026-04-26 18:26:07 -0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import { DashboardShell } from "../../components/dashboard-shell";
-import { demoPlan } from "../../lib/demo-data";
 
 export default function DashboardPage() {
-  return <DashboardShell initialPlan={demoPlan} />;
+  return <DashboardShell />;
 }
