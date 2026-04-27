95e0e5f93a821c060802ec232b2b0d354209fa7d
Yurii2108 <urapredger@gmail.com>
2026-04-26 18:50:12 -0700

    Update page.tsx

--- app/plan/[token]/page.tsx
+++ app/plan/[token]/page.tsx
@@ -1,6 +1,5 @@
-import { demoPlan } from "../../../lib/demo-data";
+import { DashboardShell } from "../../../components/dashboard-shell";
 
-export default async function SharedPlanPage({ params }: { params: Promise<{ token: string }> }) {
-  const { token } = await params;
-  return <DashboardShell initialPlan={{ ...demoPlan, shareToken: token }} sharedView />;
+export default function SharedPlanPage() {
+  return <DashboardShell />;
 }
